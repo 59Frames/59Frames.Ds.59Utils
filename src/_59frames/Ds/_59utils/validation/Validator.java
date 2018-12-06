42e4a07df964bd454c53705436bf35c94c1720ad
danielseifert <info@danielseifert.ch>
2018-12-06 16:23:26 +0100

    implemented ip validation

--- src/_59frames/Ds/_59utils/validation/Validator.java
+++ src/_59frames/Ds/_59utils/validation/Validator.java
@@ -5,10 +5,13 @@ import org.jetbrains.annotations.NotNull;
 import java.util.regex.Pattern;
 
 public final class Validator {
-    private static final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern IPV4_PATTERN = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+    private static final Pattern IPV6_PATTERN = Pattern.compile("^([\\dA-F]{1,4}:|((?=.*(::))(?!.*\\3.+\\3))\\3?)([\\dA-F]{1,4}(\\3|:\\b)|\\2){5}(([\\dA-F]{1,4}(\\3|:\\b|$)|\\2){2}|(((2[0-4]|1\\d|[1-9])?\\d|25[0-5])\\.?\\b){4})\\z");
+
 
     public static boolean isEmail(String email) {
-        return Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE).matcher(email).matches();
+        return EMAIL_PATTERN.matcher(email).matches();
     }
 
     public static boolean isPassword(String password) {
@@ -33,6 +36,14 @@ public final class Validator {
 
     public static boolean isNumber(Object number) {
         return isNumber(String.valueOf(number), NumberType.DOUBLE);
+    }
+
+    public static boolean isIpv4(String str) {
+        return IPV4_PATTERN.matcher(str).matches();
+    }
+
+    public static boolean isIpv6(String str) {
+        return IPV6_PATTERN.matcher(str).matches();
     }
 
 }
