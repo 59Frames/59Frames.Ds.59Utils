53da811dc335e2d12391fe8c5a7d15d76b431cfe
Daniel Seifert <daniel.seifert@nodflow.ch>
2018-12-23 19:26:23 +0100

    implemented interfaceable

---
+++ src/main/java/_59frames/Ds/_59utils/interfaceable/Startable.java
@@ -0,0 +1,14 @@
+package _59frames.Ds._59utils.interfaceable;
+
+/**
+ * Startable is a functional interface just to simplify
+ * programming whenever a class needs to be "started"
+ *
+ * @author Daniel Seifert
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@FunctionalInterface
+public interface Startable {
+    void start();
+}
--- src/main/java/_59frames/Ds/_59utils/tool/ThreadTools.java
+++ src/main/java/_59frames/Ds/_59utils/tool/ThreadTools.java
@@ -1,5 +1,9 @@
 package _59frames.Ds._59utils.tool;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
 public final class ThreadTools {
     public static void sleep(long millis) {
         try {
@@ -7,5 +11,13 @@ public final class ThreadTools {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void startAll(@NotNull Collection<? extends Thread> c) {
+        for (Thread t : c) t.start();
+    }
+
+    public static void waitFor(@NotNull Collection<? extends Thread> c) throws InterruptedException {
+        for(Thread t : c) t.join();
     }
 }
